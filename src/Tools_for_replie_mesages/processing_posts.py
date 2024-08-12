ddd7b49da66c4acadc0c7a27494bd3de7b04eb47
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-12 14:44:03 +0300

    Написал пару методов в класс ReplierEngine в replie_engine.py, а также написал функцию processing в файле processing_posts.py

---
+++ src/Tools_for_replie_mesages/processing_posts.py
@@ -0,0 +1,40 @@
+from src.databases.mongodb import client_mongodb
+from telethon.types import Message
+from telethon import TelegramClient
+import re
+from typing import List
+
+
+async def processing(client_session: TelegramClient, post: Message, photo_or_video: str, channel_to_post: str) -> dict:
+    """
+    Основной задачей функции является обработка постов перед их отправкой, а именно
+    замена ссылок на новые ссылки, сохранение пути фото/видео, обнаружение спам сообщений
+    :param client_session: сессия telethon
+    :param post: объект Message
+    :param photo_or_video: 'photo' or 'video'
+    :param channel_to_post: url ссылка на канал, куда отправлять сообщения
+    :return: словарь из данных
+    """
+    dict_of_data: dict = {}
+    mime_type: str = post.media.document.mime_type.split('/')[0]
+    message_text: str = post.message if len(post.message.split('\n')) != 0 else post.message + '\n'
+    if check_post_on_media(mime_type, photo_or_video) and check_post_on_advert(message_text):
+        entity_channel_to_post = await client_session.get_entity(channel_to_post)
+        path_document = await client_session.download_media(post)
+        message_text_split = message_text.split('\n')[-1]
+        message_text = message_text.replace(message_text_split, f'<a href="{channel_to_post}">{entity_channel_to_post.title}')
+
+
+def check_post_on_media(mime_type: str, type_of_media: str) -> bool:
+    if re.search(type_of_media, mime_type):
+        return True
+    else:
+        return False
+
+
+def check_post_on_advert(message: str) -> bool:
+    stop_words: List[str] = client_mongodb.get_entry(client_mongodb.collection_for_parser_configs)['stop_words']
+    for word in stop_words:
+        if re.search(word.lower(), message.lower()):
+            return False
+    return True
--- src/Tools_for_replie_mesages/replie_engine.py
+++ src/Tools_for_replie_mesages/replie_engine.py
@@ -1,5 +1,8 @@
 from telethon import TelegramClient
+from telethon.types import Message
 from src.databases.mongodb import client_mongodb
+from typing import Union
+from datetime import datetime
 
 
 class ReplierEngine:
@@ -8,4 +11,11 @@ class ReplierEngine:
         self.client_session.start()
         self.channels_from_get_the_posts = client_mongodb.get_channels_url('from')
         self.channels_to_posts_the_posts = client_mongodb.get_channels_url('to')
+
+    async def send_post_in_channel(self, channel_to_post: str, data_for_post: dict) -> None:
+        await self.client_session.send_message(channel_to_post, **data_for_post)
+
+    async def get_post_from_channel(self, channel_from_to_get_post: str, offset_date: Union[datetime, None] = None, reverse=False) -> Message:
+        post = await self.client_session.get_messages(channel_from_to_get_post, offset_date=offset_date, reverse=reverse)
+        return post
 
