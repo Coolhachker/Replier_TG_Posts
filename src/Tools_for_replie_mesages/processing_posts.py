56ef3e764364a2c4395a9a3cbbda97af35870456
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-13 13:25:07 +0300

    дописал функцию processing в processing_posts.py

--- src/Tools_for_replie_mesages/processing_posts.py
+++ src/Tools_for_replie_mesages/processing_posts.py
@@ -18,11 +18,19 @@ async def processing(client_session: TelegramClient, post: Message, photo_or_vid
     dict_of_data: dict = {}
     mime_type: str = post.media.document.mime_type.split('/')[0]
     message_text: str = post.message if len(post.message.split('\n')) != 0 else post.message + '\n'
+    emoji = client_mongodb.get_emoji(channel_to_post)
     if check_post_on_media(mime_type, photo_or_video) and check_post_on_advert(message_text):
         entity_channel_to_post = await client_session.get_entity(channel_to_post)
         path_document = await client_session.download_media(post)
         message_text_split = message_text.split('\n')[-1]
-        message_text = message_text.replace(message_text_split, f'<a href="{channel_to_post}">{entity_channel_to_post.title}')
+        message_text = message_text.replace(message_text_split, f'<a href="{channel_to_post}">{entity_channel_to_post.title}{emoji}')
+
+        dict_of_data['path_document'] = path_document
+        dict_of_data['message_text'] = message_text
+
+        return dict_of_data
+    else:
+        pass
 
 
 def check_post_on_media(mime_type: str, type_of_media: str) -> bool:
