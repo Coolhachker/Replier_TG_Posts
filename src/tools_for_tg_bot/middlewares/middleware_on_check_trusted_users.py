a64cd2b387411cdace1bdfd67faafb86c17e1759
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-16 17:56:46 +0300

    Написал пару кнопок, хендлеров команд и текста, планирую писать очередь rabbitmq

--- src/tools_for_tg_bot/middlewares/middleware_on_check_trusted_users.py
+++ src/tools_for_tg_bot/middlewares/middleware_on_check_trusted_users.py
@@ -1,9 +1,15 @@
 from aiogram import BaseMiddleware, Bot
+from src.databases.mysqldb import client_mysqldb
 
 
 class MiddlewareOnTrustUser(BaseMiddleware):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    def __call__(self, handler, event, data):
-        pass
+    async def __call__(self, handler, event, data):
+        client_mysqldb.cursor.execute('SELECT user_nickname FROM trusted_users')
+        trusted_users = [user[0] for user in client_mysqldb.cursor.fetchall()]
+        if event.from_user.username in trusted_users:
+            await handler(event, data)
+        else:
+            await self.bot.send_message(event.chat.id, '❌️ У вас нет прав на использование этого бота.')
