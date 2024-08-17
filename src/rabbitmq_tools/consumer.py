2a73d8e4ee18d628843cc59ae6e04d47dccad611
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-17 14:40:48 +0300

    fixup!

---
+++ src/rabbitmq_tools/consumer.py
@@ -0,0 +1,46 @@
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+from pika.spec import Basic, BasicProperties
+from typing import Any
+from src.databases.mongodb import client_mongodb
+
+
+class Consumer:
+    def __init__(self, host):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+
+        self.queue = 'parser'
+        self.queue_callback = 'parser_callback'
+        self.exchange = ''
+
+        self.declare_queue()
+
+        self.consume()
+
+    def declare_queue(self):
+        self.channel.queue_declare(queue=self.queue, durable=True)
+        self.channel.queue_declare(queue=self.queue_callback, durable=True)
+
+    def callback(self, channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: Any):
+        self.confirm_the_request(channel, method, properties, body)
+
+    def consume(self):
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=self.queue, on_message_callback=self.callback)
+        self.channel.start_consuming()
+
+    def confirm_the_request(self, channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
+        status_of_parser: str = client_mongodb.get_status_of_parser()
+        channel.basic_publish(
+            exchange=self.exchange,
+            routing_key=properties.reply_to,
+            body=status_of_parser.encode(),
+        )
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    def publish(self, body: bytes):
+        self.channel.basic_publish(self.exchange, routing_key=self.queue_callback, body=body)
+
+
+consumer = Consumer('localhost')
---
+++ src/rabbitmq_tools/producer_commands.py
@@ -0,0 +1,4 @@
+TURN_ON_COMMAND = 'TURN_ON'     # команда включает парсер
+TURN_OFF_COMMAND = 'TURN_OFF'   # команда выключает парсер
+ADD_OR_DELETE_CHANNELS_COMMAND = 'ADD_OR_DELETE_CHANNELS'   #команда добавляет или удаляет каналы в конфиг коллекции
+CHANGE_SETTINGS_COMMAND = "CHANGE_SETTINGS"     # команда изменяет настройки в конфиг коллекции
