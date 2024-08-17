194f12fca5fb0404ec3366e837ebbd598f646d38
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-17 14:43:29 +0300

    Сделал датакласс для команд producer

--- src/rabbitmq_tools/producer_commands.py
+++ src/rabbitmq_tools/producer_commands.py
@@ -1,4 +1,9 @@
-TURN_ON_COMMAND = 'TURN_ON'     # команда включает парсер
-TURN_OFF_COMMAND = 'TURN_OFF'   # команда выключает парсер
-ADD_OR_DELETE_CHANNELS_COMMAND = 'ADD_OR_DELETE_CHANNELS'   #команда добавляет или удаляет каналы в конфиг коллекции
-CHANGE_SETTINGS_COMMAND = "CHANGE_SETTINGS"     # команда изменяет настройки в конфиг коллекции
+from dataclasses import dataclass
+
+
+@dataclass
+class Commands:
+    TURN_ON_COMMAND: str = 'TURN_ON'     # команда включает парсер
+    TURN_OFF_COMMAND: str = 'TURN_OFF'   # команда выключает парсер
+    ADD_OR_DELETE_CHANNELS_COMMAND: str = 'ADD_OR_DELETE_CHANNELS'   #команда добавляет или удаляет каналы в конфиг коллекции
+    CHANGE_SETTINGS_COMMAND: str = "CHANGE_SETTINGS"     # команда изменяет настройки в конфиг коллекции
