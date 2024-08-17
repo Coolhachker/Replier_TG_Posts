d35a66b209f3b53d03e8d4ed1fbbc9f01a0243e7
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-17 12:33:12 +0300

    Пара настроек в rabbitmq

--- src/exceptions/castom_exceptions.py
+++ src/exceptions/castom_exceptions.py
@@ -10,3 +10,7 @@ class Exceptions:
     class ExceptionOnDateOfPost(Exception):
         def __init__(self, message):
             super().__init__(message)
+
+    class UnSuccessfulResponseRMQ(Exception):
+        def __init__(self, message):
+            super().__init__(message)
--- src/rabbitmq_tools/consumer.py
+++ src/rabbitmq_tools/consumer.py
@@ -2,6 +2,7 @@ import pika
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
 from typing import Any
+from src.databases.mongodb import client_mongodb
 
 
 class Consumer:
@@ -10,6 +11,7 @@ class Consumer:
         self.channel = self.connection.channel()
 
         self.queue = 'parser'
+        self.queue_callback = 'parser_callback'
         self.exchange = ''
 
         self.declare_queue()
@@ -18,17 +20,26 @@ class Consumer:
 
     def declare_queue(self):
         self.channel.queue_declare(queue=self.queue, durable=True)
+        self.channel.queue_declare(queue=self.queue_callback, durable=True)
 
     def callback(self, channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: Any):
-        print(type(method))
-        print(type(properties))
-        print(type(body))
-        print(body)
-        channel.basic_ack(delivery_tag=method.delivery_tag)
+        self.confirm_the_request(channel, method, properties, body)
 
     def consume(self):
         self.channel.basic_consume(queue=self.queue, on_message_callback=self.callback)
         self.channel.start_consuming()
+
+    def confirm_the_request(self, channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
+        status_of_parser: str = client_mongodb.get_status_of_parser()
+        channel.basic_publish(
+            exchange=self.exchange,
+            routing_key=properties.reply_to,
+            body=status_of_parser.encode(),
+        )
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    def publish(self, body: bytes):
+        self.channel.basic_publish(self.exchange, routing_key=self.queue_callback, body=body)
 
 
 consumer = Consumer('localhost')
