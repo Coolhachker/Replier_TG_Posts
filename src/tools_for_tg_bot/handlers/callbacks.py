c8dad034f6d141498cc6e9652046dea1cd0b82ee
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-17 14:47:59 +0300

    Первое использование команды для producer

--- src/tools_for_tg_bot/handlers/callbacks.py
+++ src/tools_for_tg_bot/handlers/callbacks.py
@@ -1,11 +1,12 @@
 from aiogram import Dispatcher, Bot
 from aiogram.types import CallbackQuery
 from src.rabbitmq_tools.producer import producer
+from src.rabbitmq_tools.producer_commands import Commands
 
 
 def callbacks(dispatcher: Dispatcher, bot: Bot):
     @dispatcher.callback_query(lambda cq: cq.data == 'start_parser')
     async def callback_on_start_parser(cq: CallbackQuery):
         await bot.delete_message(cq.message.chat.id, cq.message.message_id)
-        result = producer.publish('Turn on')
+        result = producer.publish(Commands.TURN_ON_COMMAND)
         await bot.send_message(cq.message.chat.id, result)
