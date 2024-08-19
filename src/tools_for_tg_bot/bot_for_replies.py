1d462cb5e7902d81f1b625da2f8e1af75570ff5e
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-19 14:14:47 +0300

    Добавил middleware для callback запросов

--- src/tools_for_tg_bot/bot_for_replies.py
+++ src/tools_for_tg_bot/bot_for_replies.py
@@ -4,6 +4,7 @@ from src.tools_for_tg_bot.handlers.text import text
 from src.tools_for_tg_bot.handlers.callbacks import callbacks
 from aiogram import Bot, Dispatcher
 from src.tools_for_tg_bot.middlewares.middleware_on_check_trusted_users import MiddlewareOnTrustUser
+from src.tools_for_tg_bot.middlewares.middleware_on_callback_requests import MiddlewareOnCallback
 from src.tools_for_tg_bot.handlers.rabbitmq_handler import handler_info_responses
 
 
@@ -21,6 +22,7 @@ class BotForReplies:
     async def start_pooling(self):
         asyncio.create_task(handler_info_responses(self.bot))
         self.dispatcher.message.middleware(MiddlewareOnTrustUser(self.bot))
+        self.dispatcher.callback_query.middleware(MiddlewareOnCallback(self.bot))
         await self.dispatcher.start_polling(self.bot)
 
 
--- src/tools_for_tg_bot/handlers/callbacks.py
+++ src/tools_for_tg_bot/handlers/callbacks.py
@@ -7,6 +7,10 @@ from src.rabbitmq_tools.producer_commands import Commands
 def callbacks(dispatcher: Dispatcher, bot: Bot):
     @dispatcher.callback_query(lambda cq: cq.data == 'start_parser')
     async def callback_on_start_parser(cq: CallbackQuery):
-        await bot.delete_message(cq.message.chat.id, cq.message.message_id)
         result = producer.publish(Commands.TURN_ON_COMMAND)
+        await bot.send_message(cq.message.chat.id, result)
+
+    @dispatcher.callback_query(lambda cq: cq.data == 'stop_parser')
+    async def callback_on_stop_parser(cq: CallbackQuery):
+        result = producer.publish(Commands.TURN_OFF_COMMAND)
         await bot.send_message(cq.message.chat.id, result)
