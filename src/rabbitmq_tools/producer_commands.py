39c9ca624fb6326f74a537ed9e1c4d43db8c982d
_True_developer_ <74011549+Coolhachker@users.noreply.github.com>
2024-08-20 12:16:09 +0300

    Добавил команду проверки парсера в producer_commands.py

--- src/rabbitmq_tools/producer_commands.py
+++ src/rabbitmq_tools/producer_commands.py
@@ -7,3 +7,4 @@ class Commands:
     TURN_OFF_COMMAND: str = 'TURN_OFF'   # команда выключает парсер
     ADD_OR_DELETE_CHANNELS_COMMAND: str = 'ADD_OR_DELETE_CHANNELS'   #команда добавляет или удаляет каналы в конфиг коллекции
     CHANGE_SETTINGS_COMMAND: str = "CHANGE_SETTINGS"     # команда изменяет настройки в конфиг коллекции
+    CHECK_PARSER_COMMAND: str = 'CHECK_PARSER'
